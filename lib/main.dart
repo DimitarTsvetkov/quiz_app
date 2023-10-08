ea6e1108ca39b4cd37e2c5e97d1091173459d14f
Dimitar <dimitartsvetkov89@gmail.com>
2023-10-08 12:05:59 +0300

    [us-2]-questions_screen-set-up

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 64, 35, 143),
-              Color.fromARGB(255, 94, 54, 202),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
